959bd0af074e12f000c97f25d0381ee252a486f4
Prajwal Sharma <prjwl4@gmail.com>
2023-07-15 01:24:06 +0530

    feat: add husky

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { Counter } from './components/Counter'
 
 export const App = () => {
   const callApi = async () => {
-    const a = 123
     const response = await fetch('https://jsonplaceholder.typicode.com/posts/1')
     const data = await response.json()
     alert(JSON.stringify(data, null, 2))
